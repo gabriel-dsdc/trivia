035a5846f702919c30f5726f0d18d34b39921cea
yan chapetta <yanchapetta@pop-os.localdomain>
2022-06-30 17:30:27 -0300

    ajeitando css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -36,42 +36,50 @@ class Login extends Component {
     render() {
       const { name, email, isDisable } = this.state;
       return (
-        <form>
-          <input
-            name="name"
-            type="text"
-            data-testid="input-player-name"
-            value={ name }
-            placeholder="Nome"
-            onChange={ this.handleChange }
-          />
+        <div className="conteiner">
+          <form>
+            <div id="input-conteiner">
+              <input
+                name="name"
+                className="name-input"
+                type="text"
+                data-testid="input-player-name"
+                value={ name }
+                placeholder="Nome"
+                onChange={ this.handleChange }
+              />
 
-          <input
-            name="email"
-            type="email"
-            data-testid="input-gravatar-email"
-            value={ email }
-            placeholder="Email"
-            onChange={ this.handleChange }
-          />
+              <input
+                name="email"
+                className="email-input"
+                type="email"
+                data-testid="input-gravatar-email"
+                value={ email }
+                placeholder="Email"
+                onChange={ this.handleChange }
+              />
+              <button
+                className="play-button"
+                type="submit"
+                data-testid="btn-play"
+                disabled={ isDisable }
+                onClick={ this.handleClick }
+              >
+                Play
+              </button>
+              <Link to="/settings">
+                <button
+                  className="settings-button"
+                  type="button"
+                  data-testid="btn-settings"
+                >
+                  Configurações
+                </button>
+              </Link>
 
-          <button
-            type="submit"
-            data-testid="btn-play"
-            disabled={ isDisable }
-            onClick={ this.handleClick }
-          >
-            Play
-          </button>
-          <Link to="/settings">
-            <button
-              type="button"
-              data-testid="btn-settings"
-            >
-              Configurações
-            </button>
-          </Link>
-        </form>
+            </div>
+          </form>
+        </div>
       );
     }
 }
---
+++ src/tests/Login.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import App from "../App"
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+describe('Tela de Login', () => {
+  const PLAYER_NAME = 'Nome da pessoa';
+  const PLAYER_EMAIL = 'email@pessoa.com';
+  const INPUT_PLAYER_NAME_SELECTOR = 'input-player-name';
+  const INPUT_PLAYER_EMAIL_SELECTOR = 'input-gravatar-email';
+  
+  test('Verifica se tudo se encontra na tela', () => {
+    renderWithRouterAndRedux(<App/>);
+    const nameInput = screen.getByTestId(INPUT_PLAYER_NAME_SELECTOR);
+    const emailInput = screen.getByTestId(INPUT_PLAYER_EMAIL_SELECTOR);
+    const playButtonEl = screen.getByRole('button', {name: /play/i});
+    const settingsButtonEl = screen.getByRole('button', {name: /configurações/i});
+    expect(nameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(playButtonEl).toBeInTheDocument();
+    expect(settingsButtonEl).toBeInTheDocument();
+    userEvent.type(nameInput, PLAYER_NAME);
+    userEvent.type(emailInput, PLAYER_EMAIL);
+    expect(nameInput.value).toBe(PLAYER_NAME);
+    expect(emailInput.value).toBe(PLAYER_EMAIL);
+  })
+  test("Verifica se o botão 'Play' está desabilitado", () => {
+    renderWithRouterAndRedux(<App/>);
+    const nameInput = screen.getByTestId(INPUT_PLAYER_NAME_SELECTOR);
+    const emailInput = screen.getByTestId(INPUT_PLAYER_EMAIL_SELECTOR);
+    const playButtonEl = screen.getByRole('button', {name: /play/i});
+    expect(playButtonEl).toBeDisabled();
+    userEvent.type(nameInput, PLAYER_NAME);
+    expect(playButtonEl).toBeDisabled();
+    userEvent.type(emailInput, PLAYER_EMAIL);
+    expect(playButtonEl).not.toBeDisabled();
+  })
+  test("Verifica se ao apertar em 'Play' é redirecionado", () => {
+    const {history} = renderWithRouterAndRedux(<App/>);
+    const nameInput = screen.getByTestId(INPUT_PLAYER_NAME_SELECTOR);
+    const emailInput = screen.getByTestId(INPUT_PLAYER_EMAIL_SELECTOR);
+    const playButtonEl = screen.getByRole('button', {name: /play/i});
+    userEvent.type(nameInput, PLAYER_NAME);
+    userEvent.type(emailInput, PLAYER_EMAIL);
+    userEvent.click(playButtonEl);
+    waitFor(() => {
+      expect(history.location.pathname).toBe('/game');
+    });
+  })
+  test("Verifica se ao apertar em 'Configurações' vai para a página certa", () => {
+    const {history} = renderWithRouterAndRedux(<App/>);
+    const settingsButtonEl = screen.getByRole('button', {name: /configurações/i});
+    expect(settingsButtonEl).toBeInTheDocument();
+    userEvent.click(settingsButtonEl);
+    expect(history.location.pathname).toBe('/settings');
+    
+    const settingsTitleEl = screen.getByRole('heading', {name: /configurações/i})
+    expect(settingsTitleEl).toBeInTheDocument();
+  })
+})
