1cf1e2effda7965bd396113e2b8fc118d74e517d
Gabriel dos Santos Duarte Carvalhães <100052594+gabriel-dsdc@users.noreply.github.com>
2022-07-01 17:36:24 -0300

    Delete src/Redux directory

--- src/Redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import playerReducer from './playerReducer';
-
-const rootReducer = combineReducers({
-  player: playerReducer,
-});
-
-export default rootReducer;
