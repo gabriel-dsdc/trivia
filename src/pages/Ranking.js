a87c97ea16bc376c8c220200c23b418949a6e65e
ThalesJC <thales.soul96@gmail.com>
2022-07-04 19:38:32 -0300

    Requisito 18 completo

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking:</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking:</h1>
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+            type="button"
+          >
+            Play Again
+          </button>
+        </Link>
+      </>
     );
   }
 }
